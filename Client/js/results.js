8ffd99f2d3c30592f383a64900b80720f375391b
shani24levi <48565585+shani24levi@users.noreply.github.com>
2020-12-18 12:35:22 +0200

    asve js results

--- Client/js/results.js
+++ Client/js/results.js
@@ -4,12 +4,12 @@ window.onload = () => {
 
 
 class User {
-    constructor(_parent,_id, _fname, _lname, _subject, _i) {
+    constructor(_parent,_id, _fname, _lname, _match, _i) {
         this.parent = _parent;
         this.id = _id;
         this.fname = _fname;
         this.lname = _lname;
-        this.subject = _subject;
+        this.match = _match;
         this.i =_i+1;
     }
     render() {
@@ -20,28 +20,8 @@ class User {
         <th scope="row">${this.i}</th>
         <td>${this.fname}</td>
         <td>${this.lname}</td>
-        <td>${this.subject}</td>
+        <td>${this.match}</td>
          `)
-
-        //  let trDiv = $("<tr></tr>");
-        //  $(newDiv).append(trDiv);
-
-        // let btnEdit = $(`<button class="btn btn-warning px-2"  data-light="light"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>`);
-        //   $(trDiv).append(btnEdit);
-        //   $(btnEdit).on("click",() => {
-        //     window.location.href = "edit.html"
-        //     //editAction(this.id);
-        // })
-
-        // let btnDelete = $(`<button class="btn btn btn-danger px-2"><i class="fa fa-trash" aria-hidden="true"></i></button>`);
-        // $(trDiv).append(btnDelete);
-        // $(btnDelete).on("click",() => {
-        //     let ans = alert('Are you sure you want to delete the item?');
-        //     if(ans){
-        //         console.log(this.id);
-        //         //deleteAction(this.id);
-        //     }
-    //   })
     }
 }
 
@@ -58,7 +38,7 @@ const createUser = (_ar) => {
     if (_ar.data.length > 0) {
         console.log("ar",_ar.data);
         _ar.data.map((item,i) => {
-            let mov = new User("main table tbody", item._id, item.user, item.email,i);
+            let mov = new User("main table tbody", item._id, item.fname, item.lname,item.match,i);
             mov.render();
         })
     }
