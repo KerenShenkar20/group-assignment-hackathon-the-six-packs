b125975c979e9bf803794620d2a969e760d508e0
dudital1 <dudital1@gmail.com>
2020-12-18 13:02:03 +0200

    rest working!

--- Controllers/user.ctrl.js
+++ Controllers/user.ctrl.js
@@ -1,50 +1,11 @@
-// Change the name of the file if needed
-const { userModel } = require("../Models/user");
-const { validUser } = require("../validation/users");
+const User = require('../Models/user');
 
 
-const getUsers = async (req, res) => {
-    try {
-        userModel.find({})
-            .then(data => { res.json(data); })
-            .catch(err => { res.status(400).json(err); })
-    }
-    catch (err) {
-        res.status(500).json({
-            status: 500,
-            message: err.message,
-        })
-    }
-}
+exports.userController = {
+    getUsers(req, res) {
+        User.find({})
+            .then(docs => { res.json(docs) })
+            .catch(err => console.log(`Error getting the data from DB: ${err}`));
+    },
 
-const userAdd = async (req, res) => {
-    try {
-        let valid = validUser(req.body);
-        if (!valid.error) {
-            try {
-                let data = await userModel.insertMany([req.body]);
-                res.json(data)
-            }
-            catch (err) {
-                res.status(400).json({ message: "user already in system ", code: "duplicate" });
-            }
-        }
-        else {
-            res.status(400).json(valid.error.details);
-        }
-    }
-    catch (err) {
-        res.status(500).json({
-            status: 500,
-            message: err.message,
-        })
-    }
-}
-
-module.exports = {
-    getUsers,
-    userAdd
 };
-
-
-
--- Routers/user.router.js
+++ Routers/user.router.js
@@ -1,13 +1,13 @@
 const { Router } = require('express');
-const {getUsers,userAdd} = require('../Controllers/user.ctrl');
+const {userController} = require('../Controllers/user.ctrl');
 
 
 const userRouter = new Router();
 
-userRouter.get('/',getUsers); 
+userRouter.get('/',userController.getUsers); 
 // userRouter.get('/filter',userController.filterUsers); 
 // userRouter.get('/:id',userController.getUser);
-userRouter.post('/',userAdd); 
+// userRouter.post('/',userAdd); 
 // userRouter.put('/:id',userController.updateUser); 
 // userRouter.delete('/:id',userController.deleteUser);
 
